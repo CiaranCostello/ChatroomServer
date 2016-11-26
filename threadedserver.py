4e53f2aac5847bc3bc6c1c4d4a7fef1e04ad3d71
CiaranCostello <costelci@tcd.ie>
2016-11-26 17:37:11 +0000

    changed scoping
    Signed-off-by: CiaranCostello <costelci@tcd.ie>

--- threadedserver.py
+++ threadedserver.py
@@ -5,6 +5,26 @@ from urllib.request import urlopen
 
 std_ID = 13321463
 
+# Message string generators
+def joined_message(chatroom_name, room_ref, join_id):
+	return "JOINED_CHATROOM: {0}\nSERVER_IP: {1}\nPORT: {2}\nROOM_REF: {3}\nJOIN_ID: {4}".format(chatroom_name, self.ip, self.port, room_ref, join_id)
+
+def error_message(error_code, error_description):
+	return "ERROR_CODE: {0}\nERROR_DESCRIPTION: {1}".format(error_code, error_description)
+
+def left_message(room_ref, join_id):
+	return "LEFT_CHATROOM: {0}\nJOIN_ID: {1}".format(room_ref, join_id)
+
+def disconnect_message(client_name):
+	return "DISCONNECT: 0\nPORT: 0\nCLIENT_NAME: {0}".format(client_name)
+
+def chat_message_spread(room_ref, client_name, message):
+	return "CHAT: {}\nCLIENT_NAME: {}\nMESSAGE: {}\n\n".format(room_ref, client_name, message)
+
+#takes "SOMETHING: parameter" and returns " parameter"
+def parameter(s):
+	return s.split(':')[1]
+
 class Server(object):
 
 	max_workers = 16
@@ -24,27 +44,6 @@ class Server(object):
 		self.room_ref_seed = 0
 		self.rooms = {}
 		self.roomsLock = threading.Lock()
-
-	
-	# Message string generators
-	def joined_message(chatroom_name, room_ref, join_id):
-		return "JOINED_CHATROOM: {0}\nSERVER_IP: {1}\nPORT: {2}\nROOM_REF: {3}\nJOIN_ID: {4}".format(chatroom_name, self.ip, self.port, room_ref, join_id)
-
-	def error_message(error_code, error_description):
-		return "ERROR_CODE: {0}\nERROR_DESCRIPTION: {1}".format(error_code, error_description)
-
-	def left_message(room_ref, join_id):
-		return "LEFT_CHATROOM: {0}\nJOIN_ID: {1}".format(room_ref, join_id)
-
-	def disconnect_message(client_name):
-		return "DISCONNECT: 0\nPORT: 0\nCLIENT_NAME: {0}".format(client_name)
-
-	def chat_message_spread(room_ref, client_name, message):
-		return "CHAT: {}\nCLIENT_NAME: {}\nMESSAGE: {}\n\n".format(room_ref, client_name, message)
-
-	#takes "SOMETHING: parameter" and returns " parameter"
-	def parameter(s):
-		return s.split(':')[1]
 
 	class chatroom(object):
 
